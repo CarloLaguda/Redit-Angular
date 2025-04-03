57ede029625399ec5085f95057757953766791ea
CarloLaguda <carlo.laguda07@gmail.com>
2025-04-03 10:56:13 +0200

    Buonissima base

--- redit-angular/src/app/app.component.ts
+++ redit-angular/src/app/app.component.ts
@@ -1,21 +1,33 @@
 import { Component } from '@angular/core';
-import { RouterOutlet } from '@angular/router';
+import { Article } from './article/article.model'; // <-- import this
+import { ArticleComponent } from './article/article.component';
 
 @Component({
   selector: 'app-root',
-  imports: [RouterOutlet],
+  imports:[ArticleComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.css'
+  styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  
+  title = "CREDIT"
+  articles:Article[];   // <-- component property
+  constructor(){
+    this.articles = [
+      new Article('Angular 2', 'http://angular.io', 3),
+      new Article('Fullstack', 'http://fullstack.io', 2),
+      new Article('Angular Homepage', 'http://angular.io', 1),
+    ];
+  }
+
   addArticle(title: HTMLInputElement, link: HTMLInputElement): boolean {
-    let t = title.value;
-    let v= link.value;
-    console.log(`Adding article title: `+t+ ` and link: `+ v);
+    console.log(`Adding article title: ${title.value} and link: ${link.value}`);
+    this.articles.push(new Article(title.value, link.value, 0));
+    title.value = '';
+    link.value = '';
     return false;
   }
-  
 
-  title = 'Creddit';
+  sortedArticles(): Article[] {
+    return this.articles.sort((a: Article, b: Article) => b.votes - a.votes);
+  }
 }
---
+++ redit-angular/src/app/article/article.component.ts
@@ -0,0 +1,28 @@
+import { Component, HostBinding, Input, OnInit } from '@angular/core';
+import { Article } from './article.model';
+
+@Component({
+  selector: 'app-article',
+  imports: [],
+  templateUrl: './article.component.html',
+  styleUrl: './article.component.css'
+})
+export class ArticleComponent implements OnInit {
+  @Input() article: Article
+  @HostBinding('attr.class') cssClass = 'card mt-4';
+  constructor() {
+    this.article = new Article('Angular 2','http://angular.io',10);
+  }
+
+  voteUp(): Boolean {
+    this.article.voteUp(); 
+    return false;
+  }
+
+  voteDown():Boolean {
+    this.article.voteDown();
+    return false;
+  }
+
+  ngOnInit() {}
+}
